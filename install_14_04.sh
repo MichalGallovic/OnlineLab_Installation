05500204ee1946fcc6958c5ef550738382a5009f
MichalGallovic <gallovicm@gmail.com>
2016-04-14 09:44:48 +0000

    Update install.sh

--- install_14_04.sh
+++ install_14_04.sh
@@ -14,7 +14,7 @@ debconf-set-selections <<< 'mysql-server mysql-server/root_password_again passwo
 
 echoyellow "Downloading & installing curl and python"
 apt-get install -y vim curl python-software-properties
-add-apt-repository -y ppa:ondrej/php5
+add-apt-repository -y ppa:ondrej/php5-5.6
 apt-get update
 
 echoyellow "Downloading & installing php, apache2 and mysql"
